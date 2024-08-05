c9f9dbf1482c0133653eedc398ee8b026d1dc205
pooribhajji <poorvasawant7@gmail.com>
2024-08-05 23:30:11 +0530

    EDU page created

--- projectsstyle.cs
+++ projectsstyle.cs
@@ -1,1 +1,53 @@
+*{
+    padding:0%;
+    margin:0%;
+    box-sizing: border-box;
+}
+.main{
+    background-color: #060421;
+    width:100vw;
+    height: 100vh;
+    display:flex;
+    flex-direction: column;
+    justify-content:space-evenly;
+    align-items: center;
+
+    
+}
+.project{
+    width:75vw;
+    height:35vh;
+    background-color: #f29e66;
+    text-align: center;
+    color: rgb(6, 1, 12);
+    position: relative;
+    padding-top: 30px;
+    border:10px solid red;
+    
+ 
+}
+#pro1{
+    
+    background-color:#1cfced;
+  
+    border: 5px solid red;
+    transition:all 2s steps(5);
+}
+#pro1:hover{
+    background-color:rgb(111, 247, 20);
+    background-size: cover;
+    border:none;
+}
+#pro2{
+    
+    background-color:rgb(214, 25, 88);
+  
+    border: 5px solid red;
+    transition:all 2s steps(5);
+}
+#pro2:hover{
+    background-color: #c53a0f;
+    background-size: cover;
+    border:none;
+}
 
